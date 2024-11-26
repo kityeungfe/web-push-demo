626478a3be076d405094ddb9a5170ccd20896ba1
Kit.Yeung <devkityeung@outlook.com>
2024-11-27 00:04:37 +0800

    feat: add urlBase64ToUint8Array

--- pages/api/send-notification.js
+++ pages/api/send-notification.js
@@ -11,13 +11,13 @@ webPush.setVapidDetails(
 );
 
 let subscriptions = [{
-    endpoint: 'https://fcm.googleapis.com/fcm/send/dOWNN4HXA6s:APA91bFBAAs0KRw9bJC-lx2ieWRyDh07L0R0OfHeeuXjIFETe80NtqZ7gVet1_KPz_qqd_nHIpslQlEEPgIsKIKiIYtY3fG1rRM7podCLXqdNkGLJTqmiO4okWZksu6W68cMaCU7IWcv',
-    expirationTime: null,
-    keys: {
-      p256dh: 'BCl8rv1DzLvKjpuozK6b8Jv7yp4aDOcN5Jtc0M63NY2DsdWtkfuQNrkYNm24uwg-TsGS4JTyGlKCXRuHCoisZbA',
-      auth: 'BBVlMitYRNkoDSydpN-T2A'
-    }
-  }]; // 确保和订阅 API 共用存储
+  endpoint: 'https://fcm.googleapis.com/fcm/send/dOWNN4HXA6s:APA91bFBAAs0KRw9bJC-lx2ieWRyDh07L0R0OfHeeuXjIFETe80NtqZ7gVet1_KPz_qqd_nHIpslQlEEPgIsKIKiIYtY3fG1rRM7podCLXqdNkGLJTqmiO4okWZksu6W68cMaCU7IWcv',
+  expirationTime: null,
+  keys: {
+    p256dh: 'BCl8rv1DzLvKjpuozK6b8Jv7yp4aDOcN5Jtc0M63NY2DsdWtkfuQNrkYNm24uwg-TsGS4JTyGlKCXRuHCoisZbA',
+    auth: 'BBVlMitYRNkoDSydpN-T2A'
+  }
+}]; // 确保和订阅 API 共用存储
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,29 @@ export default function Home() {
     }
   }, []);
 
+  const urlBase64ToUint8Array = (base64String) => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+      .replace(/-/g, '+')
+      .replace(/_/g, '/');
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  };
+
   const subscribe = async () => {
     if (!('serviceWorker' in navigator)) return alert('Service Worker is not supported in your browser.');
 
+    const applicationServerKey = urlBase64ToUint8Array(
+      'BBBPRNAdc92JbUOQo4kpvMLKDfKoMG2yJSxeYWary9xZ2XrH9Xprfup0A2U2c9ib5yBM2TZhHlglJxDTlq4IcVo'
+    );
     const registration = await navigator.serviceWorker.ready;
     const subscription = await registration.pushManager.subscribe({
       userVisibleOnly: true,
-      applicationServerKey: 'BBBPRNAdc92JbUOQo4kpvMLKDfKoMG2yJSxeYWary9xZ2XrH9Xprfup0A2U2c9ib5yBM2TZhHlglJxDTlq4IcVo',
+      applicationServerKey: applicationServerKey,
     });
 
     await fetch('/api/subscribe', {
